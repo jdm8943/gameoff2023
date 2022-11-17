085599aff12699084701189d42b961891d3f8fc3
Fives47 <92649317+Fives47@users.noreply.github.com>
2022-11-17 18:24:45 -0500

    Working on player input with new input system

--- Game Off 2023/Assets/Scripts/Player_Input.cs
+++ Game Off 2023/Assets/Scripts/Player_Input.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player_Input : MonoBehaviour
 {
+
+    Vector2 vel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class Player_Input : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            vel = Vector2.up * vel;
+            /*transform.rotation = transform.rotation * qPitchPlus;
+
+            rotorSpinner.setSpin(RotorSpinner.LiftMode.HIGH);
+
+            vel = Vector3.ProjectOnPlane(windSpeed * transform.up, Vector3.up);*/
+        }
     }
 }
